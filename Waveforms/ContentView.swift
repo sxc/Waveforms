e04dba36d3dca391587d215ff9427b5b56f559a6
sxc <qiuyequ@icloud.com>
2021-05-17 21:45:59 +0800

    Initial Commit

---
+++ Waveforms/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Waveforms
+//
+//  Created by Xiaochun Shen on 2021/5/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Waveforms/WaveformsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveformsApp.swift
+//  Waveforms
+//
+//  Created by Xiaochun Shen on 2021/5/17.
+//
+
+import SwiftUI
+
+@main
+struct WaveformsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
